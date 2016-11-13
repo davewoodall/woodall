f6592ca9026160f6b843d4c5095463dbed0d0e78
woodall <dave@woodalls.me>
2016-11-13 18:01:23 -0500

    add some fun facts

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ $(function() {
   $('.letter').on('mouseover', function() {
     var letter = $(this).html().toLowerCase();
     var klass = 'body-' + letter;
+    var fact = '.fact-' + letter;
     $('body').removeClass().addClass(klass);
+    $('.fun-fact').children().addClass('hidden');
+    $(fact).removeClass('hidden');
   });
 });
