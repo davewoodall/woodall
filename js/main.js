1a53ab044b7e180263f306a2c6bd268925ac5498
woodall <dave@woodalls.me>
2016-11-25 14:30:13 -0500

    consistent naming for menu open & close buttons

--- js/main.js
+++ js/main.js
@@ -1,22 +1,26 @@
 $(function() {
+
+  // DAVE actions
   $('.letter').on('mouseover', function() {
     var letter = $(this).html().toLowerCase();
     var klass = 'body-' + letter;
     var fact = '.fact-' + letter;
-
     $('body').removeClass().addClass(klass);
     $('.fun-fact').children().addClass('hidden');
     $(fact).removeClass('hidden');
   });
 
-  $('.menu-link').on('click', function() {
+  // nav-open
+  $('#menu-open').on('click', function() {
     $('.nav-box').removeClass('hidden');
     $('body').removeClass();
     $('body').addClass('drawer-out');
   });
-  
-  $('#close-menu').on('click', function() {
+
+  // nav-close
+  $('#menu-close').on('click', function() {
     $('body').removeClass('');
     $('body').addClass('drawer-in');
+    $('.nav-box').addClass('hidden');
   });
 });
