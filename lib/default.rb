3f1a860728ea551bdacfc15895a9295be31ac04b
fakefarm <dave@woodalls.me>
2017-12-01 17:25:28 -0500

    add google fonts

--- lib/default.rb
+++ lib/default.rb
@@ -3,3 +3,15 @@ include Nanoc::Helpers::Tagging
 include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::Text
+
+
+def google_fonts_loader
+  case config[:google_fonts].length
+  when 1
+    ''
+  when 2
+    %Q(<link href="https://fonts.googleapis.com/css?family=#{config[:google_fonts][0]}|#{config[:google_fonts][1]}" rel="stylesheet">)
+  else
+    ''
+  end
+end
